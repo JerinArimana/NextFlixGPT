1d884fa15de4e0dc6788bda8383c392032f31bc1
jerinexpansionjs <jerin.jose@expansionjs.com>
2024-11-11 23:21:12 +0530

    login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,70 @@
+import { useRef, useState } from "react";
 import Header from "./Header";
+import { ValidationCheck } from "../utils/validation";
 
 const Login = () => {
+  const name = useRef();
+  const email = useRef();
+  const password = useRef();
+
+  const [isLoginForm, setIsLoginForm] = useState(false);
+  const [isFormValid, setIsFormValid] = useState(null);
+  const togleSignInForm = () => {
+    setIsLoginForm(!isLoginForm);
+  };
+
+  const sumbmitHandler = (e) => {
+    e.preventDefault();
+    const message = ValidationCheck(
+      email.current.value,
+      password.current.value
+    );
+    setIsFormValid(message);
+  };
   return (
     <>
       <div>
         <Header />
-        <div>
+        <div className="absolute">
           <img
             src="https://assets.nflxext.com/ffe/siteui/vlv3/81d64f3c-9627-4741-8f74-422bf35f9f1d/web/IN-en-20241104-TRIFECTA-perspective_55263ea2-af7f-40ed-9cf0-7029a9b9baf4_small.jpg"
             alt="banner"
           />
         </div>
+        <form className="rounded-lg bg-opacity-70 mx-auto right-0 left-0 absolute  w-3/12 my-36 bg-black text-white p-12">
+          <h1>{isLoginForm ? "Sign In" : "Sign Up"}</h1>
+
+          {isLoginForm && (
+            <input
+              ref={name}
+              type="text"
+              placeholder="Full Name"
+              className="p-2 my-4 w-full"
+            />
+          )}
+          <input
+            ref={email}
+            type="text"
+            placeholder="Email Address"
+            className="p-2 my-4 w-full"
+          />
+          <input
+            ref={password}
+            type="password"
+            placeholder="Password"
+            className="p-2 my-4 w-full"
+          />
+          <p>{isFormValid}</p>
+          <button
+            onClick={sumbmitHandler}
+            className="p-4 my-4 w-full bg-red-800 rounded-lg"
+          >
+            {isLoginForm ? "Sign Up" : "Sign In"}
+          </button>
+          <p onClick={togleSignInForm}>
+            New to Nextflix? {isLoginForm ? "Sign in" : "Sign up now"}
+          </p>
+        </form>
       </div>
     </>
   );
